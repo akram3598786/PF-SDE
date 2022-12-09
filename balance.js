442067f9f2c8e544925bd6941c6eb9f739339097
akram3598786 <akramali03598786@gmail.com>
2022-12-09 16:01:09 +0530

    Complete functionalies

--- balance.js
+++ balance.js
@@ -7,21 +7,24 @@ fs.readFile("./1-input.json", (err, data) => {
     console.log(err);
   } else {
     jsonInput = JSON.parse(data);
-    // console.log(jsonInput);
     Solution(jsonInput);
   }
 })
 
 // Main Function (Solver)
-function Solution(data){
+function Solution(data) {
   let { expenseData, revenueData } = data;
 
   let Sumup_expenseData = Sum_up(expenseData);
   let Sumup_revenueData = Sum_up(revenueData);
 
-  getBalaceData(Sumup_expenseData,Sumup_revenueData)
-  //  console.log(Sumup_expenseData);
-  //  console.log(Sumup_revenueData);
+ let outData = getBalaceData(Sumup_expenseData, Sumup_revenueData);
+ let sortedData = sortMonthsData(outData);
+ let missedMonths = checkForMissedMonths(sortedData);
+ let finalbuild = computFinalData(sortedData,missedMonths);
+ let finalSorted = sortMonthsData(finalbuild);
+console.log(finalSorted);
+//  console.log(JSON.stringify(finalSorted));
 
 }
 
@@ -36,41 +39,99 @@ function Sum_up(typeofData) {
       let toadd_amount = typeofData[i].amount;
       let prev_amount = taken_dates[curdate];
       let sum_amount = prev_amount + toadd_amount;
-       taken_dates[curdate] = sum_amount;
+      taken_dates[curdate] = sum_amount;
     }
   }
   return taken_dates;
 }
 
-function getBalaceData(expenseData,revenueData){
-   let outData = [];
 
-   for(let date in revenueData){
-    let rowdata ={
+// Computing with revenue and expenses to get balance amount for each month
+function getBalaceData(expenseData, revenueData) {
+  let outData = [];
+     // Computing for months in revenueData
+  for (let date in revenueData) {
+    let rowdata = {
       amount: 0,
       startDate: date
     }
-       if(expenseData[date]){
-        rowdata.amount = revenueData[date] - expenseData[date];
-       } 
-       else {
-        // console.log("revn", date);
-        rowdata.amount = revenueData[date];
-       }
-       outData.push(rowdata);
-   }
-
-   for(let date in expenseData){
-    let rowdata ={
+    if (expenseData[date]) rowdata.amount = revenueData[date] - expenseData[date];
+    else rowdata.amount = revenueData[date];
+    outData.push(rowdata);
+  }
+    // Computing for months in expenseData (months not in revenueData)
+  for (let date in expenseData) {
+    let rowdata = {
       amount: 0,
       startDate: date
     }
-      if(!revenueData[date]){
-        rowdata.amount = -expenseData[date]; 
-        outData.push(rowdata);
+    if (!revenueData[date]) {
+      rowdata.amount = -expenseData[date];
+      outData.push(rowdata);
+    }
+  }
+  return outData;
+}
+
+// Sorting data (date wise)
+ function sortMonthsData(data) {
+  let sortedData = data.sort((x, y) => {
+
+    const get_month_nd_year = (CompDate) => {
+      let sections = CompDate.split(":");
+      let date = sections[0];
+      let dateSplit = date.split("-");
+      let year = dateSplit[0];
+      let month = dateSplit[1];
+      return [year, month];
+    }
+    const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+    let [xYear, xMonth] = get_month_nd_year(x.startDate);
+    let [yYear, yMonth] = get_month_nd_year(y.startDate);
+
+    if (xYear != yYear) return xYear - yYear;
+    else return months.indexOf(xMonth) - months.indexOf(yMonth);
+  });
+   return sortedData;
+}
+
+// Checking for missed months revenue and expenses 
+function checkForMissedMonths(sortedData) {
+  let missedMonths = [];
+  let seriesCount = 1;
+  sortedData.forEach((curRow) => {
+    let curMonth = parseInt(curRow.startDate.split("-")[1]);
+    let yr = parseInt(curRow.startDate.split("-")[0]);
+    if (curMonth - seriesCount > 0) {
+      while (curMonth - seriesCount > 0) {
+        let toadd = {
+          month: seriesCount,
+          year: yr
+        }
+        missedMonths.push(toadd);
+        seriesCount++;
       }
-     
-   }
+    } else {
+      seriesCount++;
+    }
+  });
+  return  missedMonths;
+}
 
-   console.log(outData);
+// Final adding missed months to balanced data
+function computFinalData(sortedData,missedMonths){
+
+  missedMonths.forEach((ele)=>{
+    let {month, year} = ele;
+    if(month < 10){
+      month = "0" + String(month);
+    }
+    let date = `${year}-${month}-01T00:00:00.000Z`;
+    let newRaw = {
+      amount : 0,
+      startDate : date
+    }
+    sortedData.push(newRaw);
+  })
+  return sortedData;
 }
